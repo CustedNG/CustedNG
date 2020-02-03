30cab815cc91221966cc74a91091d37ed4d51306
xuty <xty50337@hotmail.com>
2020-02-03 08:55:35 +0800

    update

--- lib/core/analytics.dart
+++ lib/core/analytics.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:countly_flutter/countly_flutter.dart';
+import 'package:custed2/core/extension/intx.dart';
+import 'package:dio/dio.dart';
 
 class Analytics {
   static const _url = '';
@@ -6,22 +10,29 @@ class Analytics {
 
   static bool _enabled = false;
 
-  static void init() async {
+  static Future<void> init() async {
     if (_url.isEmpty || _key.isEmpty) {
       return;
     }
 
     _enabled = true;
     await Countly.init(_url, _key);
-    await Countly.enableCrashReporting();
+    Countly.enableCrashReporting();
+    Countly.giveAllConsent();
   }
 
   static set isDebug(bool value) {
     Countly.isDebug = value;
+    Countly.setLoggingEnabled(value);
   }
 
   static void recordView(String view) {
     if (!_enabled) return;
     Countly.recordView(view);
+  }
+
+  static void recordException(Object exception, [bool fatal = false]) {
+    if (!_enabled) return;
+    Countly.logException(exception.toString(), !fatal, null);
   }
 }
