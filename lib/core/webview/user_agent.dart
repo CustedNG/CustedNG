e82119099ed0d8452a41b332c07487d24116ab1e
wyy <bsheep0734@gmail.com>
2020-03-05 21:43:28 +0800

    change ua

--- lib/core/webview/user_agent.dart
+++ lib/core/webview/user_agent.dart
@@ -1,5 +1,5 @@
 class UserAgent {
-  static const defaultUA =
-      'Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) '
-      'AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1';
+  static const defaultUA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36';
+      // 'Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) '
+      // 'AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1';
 }
