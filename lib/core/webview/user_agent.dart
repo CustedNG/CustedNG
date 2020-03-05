e215b244c3fb36f648c0a71c3f7be6329f08043f
xuty <xty50337@hotmail.com>
2020-03-05 20:38:09 +0800

    minor improvements

--- lib/core/webview/user_agent.dart
+++ lib/core/webview/user_agent.dart
@@ -1,6 +1,5 @@
 class UserAgent {
   static const defaultUA =
-      'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) '
-      'AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 '
-      'Safari/602.2';
+      'Mozilla/5.0 (Linux; U; Android 2.3.7; en-us; Nexus One Build/FRF91) '
+      'AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1';
 }
