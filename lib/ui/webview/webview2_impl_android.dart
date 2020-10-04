dd12548f184f663c3949549b02f8c3f61a39c9fb
Junyuan Feng <2036293523@qq.com>
2020-10-04 23:10:52 +0800

    fix webview back/forward issue

--- lib/ui/webview/webview2_impl_android.dart
+++ lib/ui/webview/webview2_impl_android.dart
@@ -1,17 +1,19 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:custed2/core/open.dart';
 import 'package:custed2/core/webview/user_agent.dart';
 import 'package:custed2/ui/webview/webview2.dart';
-import 'package:custed2/ui/webview/webview2_bottom.dart';
 import 'package:custed2/ui/webview/webview2_controller.dart';
 import 'package:custed2/ui/webview/webview2_header.dart';
 import 'package:custed2/ui/webview/webview2_plugin.dart';
+import 'package:custed2/ui/widgets/missing_icons.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:share_extend/share_extend.dart';
 import 'package:webview_cookie_manager/webview_cookie_manager.dart';
 
 StreamSubscription<T> listen<T>(Stream<T> source, void Function(T) handler) {
@@ -63,7 +65,9 @@ class Webview2StateAndroid extends Webview2State {
   final wp = FlutterWebviewPlugin();
 
   final header = Webview2HeaderController();
-  final bottom = Webview2BottomController();
+
+  bool canBack = false;
+  bool canForward = false;
 
   StreamSubscription _onDestroy;
   StreamSubscription<String> _onUrlChanged;
@@ -131,6 +135,10 @@ class Webview2StateAndroid extends Webview2State {
       pluginOnLoadStart(Webview2ControllerAndroid(), state.url);
 
       widget.onLoadStart?.call(Webview2ControllerAndroid(), state.url);
+
+      canBack = await wp.canGoBack();
+      canForward = await wp.canGoForward();
+      setState(() {});
     }
 
     if (state.type == WebViewState.finishLoad) {
@@ -204,11 +212,58 @@ class Webview2StateAndroid extends Webview2State {
         withLocalStorage: true,
         hidden: true,
         initialChild: buildLoadingWidget(),
-        bottomNavigationBar: Webview2Bottom(
-          controller: bottom,
-          url: getUrl,
-          onGoBack: wp.goBack,
-          onGoForward: wp.goForward,
+        bottomNavigationBar: BottomAppBar(
+          color: CupertinoColors.systemGroupedBackground.resolveFrom(context),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  width: 0.1,
+                  color: CupertinoColors.opaqueSeparator.resolveFrom(context),
+                ),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                IconButton(
+                    icon: canBack
+                        ? const Icon(Icons.arrow_back_ios)
+                        : const Icon(Icons.arrow_back_ios, color: Colors.white54),
+                    onPressed: () async {
+                      wp?.goBack();
+                    }
+                ),
+                IconButton(
+                  icon: canForward
+                      ? const Icon(Icons.arrow_forward_ios)
+                      : const Icon(Icons.arrow_forward_ios, color: Colors.white54),
+                  onPressed: () async {
+                    wp?.goForward();
+                  } ,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.share),
+                  onPressed: () async {
+                    // var url = await webview.evalJavascript('window.location.href');
+                    final url = await getUrl();
+                    ShareExtend.share(url, 'text');
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(MissingIcons.earth, size: 26),
+                  onPressed: () async {
+                    // var url = await webview.evalJavascript('window.location.href');
+                    // if (url.length >= 2) {
+                    //   url = url.substring(1, url.length - 1);
+                    // }
+                    final url = await getUrl();
+                    openUrl(url);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
