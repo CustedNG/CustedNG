421ef218e36da22d04931c07f049f6571267d46d
xuty <xty50337@hotmail.com>
2020-10-09 23:01:36 +0800

    fix ios open-in-browser

--- lib/ui/webview/webview2_impl_general.dart
+++ lib/ui/webview/webview2_impl_general.dart
@@ -92,6 +92,7 @@ class Webview2StateGeneral extends Webview2State {
       ),
       bottomNavigationBar: Webview2Bottom(
         controller: bottom,
+        url: () => controller?.getUrl(),
         onGoForward: () => controller?.goForward(),
         onGoBack: () => controller?.goBack(),
       ),
