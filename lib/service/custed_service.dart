684b3fd383915bfcb531e17d93d58ce0aa0cbf17
xuty <xty50337@hotmail.com>
2020-12-09 16:23:24 +0800

    use remote plugin

--- lib/service/custed_service.dart
+++ lib/service/custed_service.dart
@@ -73,6 +73,11 @@ class CustedService extends CatClient {
     return resp.body;
   }
 
+  Future<List<String>> getWebviewPlugins() async {
+    final resp = await get('https://cust.cc/webview/plugins.json');
+    return List<String>.from(json.decode(resp.body));
+  }
+
   static String getFileUrl(CustedFile file) {
     if (file == null) return null;
     return file.url.startsWith('/') ? '$baseUrl${file.url}' : file.url;
