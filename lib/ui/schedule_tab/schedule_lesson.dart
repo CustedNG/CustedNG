b84b3b85b3b039a56540e3a20a07fa9d601594d1
PaperCube <imzhy@hotmail.com>
2021-03-18 18:45:03 +0800

    Collapse excessive conflicting lectures in table cell

--- lib/ui/schedule_tab/schedule_lesson.dart
+++ lib/ui/schedule_tab/schedule_lesson.dart
@@ -57,14 +57,21 @@ class ScheduleLessonWidget extends StatelessWidget {
       content.add(SizedBox(height: 2));
       content.add(Text('@' + lesson.roomRaw, maxLines: 3));
     } else {
-      for (var lesson in conflict) {
-        content.add(SizedBox(height: 5));
-        content.add(material.Divider(
-          height: 1,
-          color: CupertinoColors.white,
-        ));
-        content.add(SizedBox(height: 5));
+      final divider = () => material.Divider(
+            height: 1,
+            color: CupertinoColors.white,
+          );
+      for (var lesson in conflict.take(1)) {
+        content.add(SizedBox(height: 3));
+        content.add(divider());
+        content.add(SizedBox(height: 3));
         content.add(Text(lesson.name, maxLines: 2));
+      }
+      if (conflict.length >= 2) {
+        content.add(SizedBox(height: 3));
+        content.add(divider());
+        final more = conflict.length - 1;
+        content.add(Text("... +${more}"));
       }
     }
 
