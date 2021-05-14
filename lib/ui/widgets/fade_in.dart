edbc085c09a5ff6148067203170b9fb707b76888
xuty <xty50337@hotmail.com>
2021-05-14 21:23:49 +0800

    run dart format

--- lib/ui/widgets/fade_in.dart
+++ lib/ui/widgets/fade_in.dart
@@ -1,39 +1,43 @@
 import 'package:flutter/material.dart';
 
-class FadeIn extends StatefulWidget {  
-    final Widget child;  
+class FadeIn extends StatefulWidget {
+  final Widget child;
 
-    FadeIn({@required this.child});  
+  FadeIn({@required this.child});
 
-    @override  _MyFadeInState createState() => _MyFadeInState();
+  @override
+  _MyFadeInState createState() => _MyFadeInState();
 }
-class _MyFadeInState extends State<FadeIn> with SingleTickerProviderStateMixin {  
-    AnimationController _controller;  
-    Animation _animation;  
 
-    @override  void initState() {    
-        super.initState();    
-        _controller = AnimationController(      
-            vsync: this,      
-            duration: Duration(milliseconds: 377),    
-        );   
-         _animation = Tween(      
-            begin: 0.0,      
-            end: 1.0,    
-        ).animate(_controller);  
-    }  
+class _MyFadeInState extends State<FadeIn> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
 
-    @override  void dispose() {    
-        super.dispose();    
-        _controller.dispose();    
-        super.dispose();  
-    } 
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 377),
+    );
+    _animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_controller);
+  }
 
-    @override  Widget build(BuildContext context) {    
-        _controller.forward();    
-        return FadeTransition(      
-            opacity: _animation,      
-            child: widget.child,    
-        );  
-    }
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _controller.forward();
+    return FadeTransition(
+      opacity: _animation,
+      child: widget.child,
+    );
+  }
 }
